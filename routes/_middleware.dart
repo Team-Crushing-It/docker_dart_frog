3ba6917a97126d91f6fffcbc91d0020f10abd0f3
Gene <yllanesgene@gmail.com>
2023-01-26 11:21:02 -0500

    feat: utilize datastore

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:in_memory_todos_data_source/in_memory_todos_data_source.dart';
+import 'package:neo4j_todos_data_source/neo4j_todos_data_source.dart';
 
-final _dataSource = InMemoryTodosDataSource();
+final _dataSource = Neo4jTodosDataSource();
 
 Handler middleware(Handler handler) {
   return handler
